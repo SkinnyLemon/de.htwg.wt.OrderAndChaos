f22c6b8fb2b2b72fd450f7cb5ae4f7583940644a
Tobias Leger <tobias.leger@htwg-konstanz.de>
2019-11-06 03:15:42 +0100

    Initial Commit

---
+++ app/controllers/GameController.scala
@@ -0,0 +1,47 @@
+package controllers
+
+import de.htwg.se.orderandchaos.OrderAndChaos
+import de.htwg.se.orderandchaos.control.Control
+import javax.inject._
+import play.api.mvc._
+
+import scala.util.{Failure, Success, Try}
+
+
+@Singleton
+class GameController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+  val control: Control = OrderAndChaos.control
+
+  def set(x: String, y: String, value: String): Action[AnyContent] = Action {
+    val error = getError(() => control.play(x.toInt, y.toInt, value))
+    Ok(views.html.orderandchaos(control.controller, error))
+  }
+
+  def undo(): Action[AnyContent] = Action {
+    val error = getError(control.undo)
+    Ok(views.html.orderandchaos(control.controller, error))
+  }
+
+  def redo(): Action[AnyContent] = Action {
+    val error = getError(control.redo)
+    Ok(views.html.orderandchaos(control.controller, error))
+  }
+
+  def reset(): Action[AnyContent] = Action {
+    val error = getError(control.reset)
+    Ok(views.html.orderandchaos(control.controller, error))
+  }
+
+  def display: Action[AnyContent] = Action {
+    Ok(views.html.orderandchaos(control.controller, ""))
+  }
+
+  def about: Action[AnyContent] = Action {
+    Ok(views.html.index())
+  }
+
+  private def getError(f: () => Unit): String = Try(f()) match {
+    case Success(_) => ""
+    case Failure(e) => e.getMessage
+  }
+}
