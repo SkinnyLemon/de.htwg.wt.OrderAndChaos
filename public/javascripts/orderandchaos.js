f9df029d5ffce69f0db5331a8c84f21cc47ba329
herrqian <qianjing1993@gmail.com>
2019-11-19 15:37:24 +0100

    modified js

--- public/javascripts/orderandchaos.js
+++ public/javascripts/orderandchaos.js
@@ -1,12 +1,12 @@
 let size = 6
 let game_json = {
-    size = 6
-    0: {0:"E",1:"E",2:"E",3:"E",4:"E",5:"E"},
-    1: {0:"E",1:"E",2:"E",3:"E",4:"E",5:"E"},
-    2: {0:"E",1:"E",2:"E",3:"E",4:"E",5:"E"},
-    3: {0:"E",1:"E",2:"E",3:"E",4:"E",5:"E"},
-    4: {0:"E",1:"E",2:"E",3:"E",4:"E",5:"E"},
-    5: {0:"E",1:"E",2:"E",3:"E",4:"E",5:"E"},
+    size : 6,
+    6: {6:"E",5:"E",4:"E",3:"E",2:"E",1:"E"},
+    5: {6:"E",5:"E",4:"E",3:"E",2:"E",1:"E"},
+    4: {6:"E",5:"E",4:"E",3:"E",2:"E",1:"E"},
+    3: {6:"E",5:"E",4:"E",3:"E",2:"E",1:"E"},
+    2: {6:"E",5:"E",4:"E",3:"E",2:"E",1:"E"},
+    1: {6:"E",5:"E",4:"E",3:"E",2:"E",1:"E"},
 };
 
 class Grid {
@@ -16,37 +16,40 @@ class Grid {
     }
 
     fill_json(json) {
-        for (let idx = 0; idx < this.size; idx++) {
-            this.cells[idx] = json[idx]:
+        for (let idx = 1; idx < this.size + 1; idx++) {
+            this.cells[idx] = json[idx];
         }
     }
 
 }
 
 let grid = new Grid(game_json.size);
-grid.fill_json(game_json)
+grid.fill_json(game_json);
 
 function fillGrid(grid) {
-    html = ""
-    for (var rows in grid.cells) {
-        for (var cell in rows) {
-            if (grid.cells[rows][cell] == "E") {
-                html = html + "<a href="/play/@x/@y/R" class="choice red-choice">" + "</a>" + "\n"
-                html = html + "<a href="/play/@x/@y/B" class="choice blue-choice">" + "</a>" + "\n"
+    html = "";
+    for (var row in grid.cells) {
+        for (var col in grid.cells[row]) {
+            if (grid.cells[row][col] == "E") {
+                html = html + "<a href=\"/play/" +row+"/" +col + "/R\" class=\"choice red-choice\" onclick='setCell("+row+","+col+",\"R\")'>" + "</a>" + "\n";
+                html = html + "<a href=\"/play/" +row+"/"+col+"/B\" class=\"choice blue-choice\" onclick='setCell("+row+","+col+",\"B\")'>" + "</a>" + "\n";
             }
+            console.log(html)
+            document.getElementById("game-cell" + row + col).innerHTML = html;
+            html = "";
         }
     }
 }
 
-fucntion setCell(x, y , value) {
+function setCell(x, y , value) {
     console.log("Setting cell ("+x+","+y+")"+"to"+value);
     grid.cells[x][y] = value;
+    document.getElementById("game-cell" + x + y).off("click");
 
 }
 
 $( document ).ready(function() {
     console.log( "Document is ready, filling grid" );
     fillGrid(grid);
-    registerClickListener();
-
+    console.log("Grid is filled")
 });
