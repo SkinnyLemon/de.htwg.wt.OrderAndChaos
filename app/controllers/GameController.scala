db8c49446ff0422398738819eb1d7ff1b153a94c
Tobias Leger <tobias.leger@htwg-konstanz.de>
2019-11-27 02:36:41 +0100

    Added Websocket and Json support

--- app/controllers/GameController.scala
+++ app/controllers/GameController.scala
@@ -1,9 +1,14 @@
 package controllers
 
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
+import akka.stream.Materializer
 import de.htwg.se.orderandchaos.OrderAndChaos
-import de.htwg.se.orderandchaos.control.{Control, CellSet, Win}
-
+import de.htwg.se.orderandchaos.control.{CellSet, Control, Win}
+import de.htwg.se.orderandchaos.control.json.JsonConverter
 import javax.inject._
+import parser.JsonExecutor
+import play.api.libs.json.{JsValue, Json}
+import play.api.libs.streams.ActorFlow
 import play.api.mvc._
 
 import scala.swing.Reactor
@@ -11,10 +16,9 @@ import scala.util.{Failure, Success, Try}
 
 
 @Singleton
-class GameController @Inject()(cc: ControllerComponents) extends AbstractController(cc) with Reactor{
+class GameController @Inject()(cc: ControllerComponents)(implicit system: ActorSystem, mat: Materializer) extends AbstractController(cc) {
   val control: Control = OrderAndChaos.control
-
-  listenTo(control)
+  val jsonExecutor = new JsonExecutor(control)
 
   def set(x: String, y: String, value: String): Action[AnyContent] = Action {
     val error = getError(() => control.play(x.toInt, y.toInt, value))
@@ -42,6 +46,37 @@ class GameController @Inject()(cc: ControllerComponents) extends AbstractControl
 
   def about: Action[AnyContent] = Action {
     Ok(views.html.index())
+  }
+
+  def socket = WebSocket.accept[String, String](_ =>  {
+    ActorFlow.actorRef(out => {
+      println("Websocket connected!")
+      OacWebsocketActor.create(out)
+    })
+  })
+
+  object OacWebsocketActor {
+    def create(out: ActorRef): Props = {
+      Props(new OacWebsocketActor(out))
+    }
+  }
+
+  class OacWebsocketActor(out: ActorRef) extends Actor with Reactor {
+    listenTo(control)
+    sendJson()
+
+    def receive: PartialFunction[Any, Unit] = {
+      case msg: String => jsonExecutor.execute(Json.parse(msg))
+    }
+
+    reactions += {
+      case _: Win => sendJson()
+      case _: CellSet => sendJson()
+    }
+
+    def sendJson(): Unit = {
+      out ! Json.stringify(JsonConverter.convertToJson(control.controller))
+    }
   }
 
   private def getError(f: () => Unit): String = Try(f()) match {
--- public/javascripts/orderandchaos.js
+++ public/javascripts/orderandchaos.js
@@ -1,56 +1,119 @@
-let game_json = {
-    size: 6,
-    6: {1: "B", 2: "B", 3: "E", 4: "E", 5: "B", 6: "R"},
-    5: {1: "B", 2: "E", 3: "E", 4: "E", 5: "E", 6: "B"},
-    4: {1: "E", 2: "E", 3: "E", 4: "E", 5: "E", 6: "E"},
-    3: {1: "E", 2: "E", 3: "E", 4: "E", 5: "E", 6: "E"},
-    2: {1: "R", 2: "E", 3: "E", 4: "E", 5: "E", 6: "R"},
-    1: {1: "R", 2: "R", 3: "E", 4: "E", 5: "B", 6: "B"}
+let status;
+const webSocket = new WebSocket("ws://localhost:9000/socket");
+webSocket.onopen = function () {
+    console.log("WebSocket opened");
+};
+webSocket.onmessage = function (message) {
+    console.log("WebSocket Message received");
+    console.log(JSON.parse(message.data));
+    status = new Status(JSON.parse(message.data));
+};
+webSocket.onerror = function () {
+    console.error("WebSocket error");
+};
+webSocket.onclose = function () {
+    console.log("WebSocket closed");
 };
 
-class Grid {
-    constructor(size) {
-        this.size = size;
-        this.cells = {};
+class Status {
+    constructor(websocket) {
+        this.loadFromJson(websocket)
     }
 
-    fill_json(json) {
-        for (let idx = 1; idx < this.size + 1; idx++) {
-            this.cells[idx] = json[idx];
+    loadFromJson(json) {
+        this.cells = json.grid.fields;
+        this.player = json.turn;
+        this.type = json.type;
+        this.updateCells();
+        this.updateTopText()
+    }
+
+    updateCells() {
+        for (const row in this.cells) {
+            for (const col in this.cells[row]) {
+                this.updateCell(row, col)
+            }
         }
     }
 
-}
+    updateCell(x, y) {
+        const cellType = this.cells[y][x].type;
+        console.log("Updating cell (" + x + "," + y + ") to " + cellType);
+        const query = $("#game-cell" + x + y);
+        if (cellType !== "R") {
+            query.removeClass("red-cell");
+        }
+        if (cellType !== "B") {
+            query.removeClass("blue-cell");
+        }
+        if (cellType !== "E" || this.type !== "standard") {
+            query.removeClass("empty-cell")
+        }
+        if (cellType !== "E" || this.type !== "game-over") {
+            query.removeClass("empty-cell-blocked")
+        }
 
-let grid = new Grid(game_json.size);
-grid.fill_json(game_json);
+        query.addClass(cellType === "B" ? "blue-cell" : cellType === "R" ? "red-cell" : this.type === "standard" ? "empty-cell" : "empty-cell-blocked");
 
-function fillGrid(grid) {
-    for (const row in grid.cells) {
-        for (const col in grid.cells[row]) {
-            setCell(row, col, grid.cells[row][col])
+        if (cellType === "E" && this.type === "standard") {
+            query.addClass("empty-cell");
+            let html = "<div class=\"choice red-choice\"/>\n";
+            html = html + "<div class=\"choice blue-choice\"/>\n";
+            query.html(html);
+            query.children().eq(0).click(function () {
+                setCell(x, y, "R")
+            });
+            query.children().eq(1).click(function () {
+                setCell(x, y, "B")
+            });
+        } else {
+            query.empty();
+        }
+    }
+
+    updateTopText() {
+        const start = this.type === "standard" ? "Turn: " : "Winner: ";
+        $("#top-text").html(start + this.player)
+    }
+
+    toJson() {
+        return {
+            "type": this.type,
+            "turn": this.player,
+            "grid": {
+                "fields": this.cells
+            }
         }
     }
 }
 
-function setCell(x, y, value) {
-    console.log("Setting cell (" + x + "," + y + ") to " + value);
-    grid.cells[x][y] = value;
-    const query = $("#game-cell" + x + y);
-    query.addClass(value === "E" ? "empty-cell" : value === "B" ? "blue-cell" : "red-cell");
-    if (value === "E") {
-        let html = "<div class=\"choice red-choice\"/>\n";
-        html = html + "<div class=\"choice blue-choice\"/>\n";
-        query.html(html);
-        query.children().eq(0).click(function() { setCell(x, y, "R") });
-        query.children().eq(1).click(function() { setCell(x, y, "B") });
-    } else {
-        query.empty();
-    }
+function setCell(x, y, type) {
+    console.log("Setting cell (" + x + "," + y + ") to " + type);
+    const message = {x, y, type, "action": "set"};
+    sendMessage(message);
+}
+
+function undo() {
+    console.log("Undoing last step");
+    const message = {"action": "undo"};
+    sendMessage(message);
+}
+
+function redo() {
+    console.log("Redoing last step");
+    const message = {"action": "redo"};
+    sendMessage(message);
+}
+
+function sendMessage(message) {
+    webSocket.send(JSON.stringify(message))
 }
 
 $(document).ready(function () {
-    console.log("Document is ready, filling grid");
-    fillGrid(grid);
-    console.log("Grid is filled")
+    $("#undo-button").click(function () {
+        undo()
+    });
+    $("#redo-button").click(function () {
+        redo()
+    });
 });
