7c59c7b129fb246fdc6798ef17bd19247a67d03a
SkinnyLemon <tobias.leger@htwg-konstanz.de>
2019-12-18 19:01:45 +0100

    Moved js into polymer class and added dependency for polymer-cli

--- public/polymer/src/order-and-chaos.js
+++ public/polymer/src/order-and-chaos.js
@@ -7,12 +7,71 @@ export class OrderAndChaos extends LitElement {
         super();
         this.rows = [ '1', '2', '3', '4', '5', '6' ];
         this.columns = [ '1', '2', '3', '4', '5', '6' ];
+        this.cells = [[{value: "E"}, {value: "E"}, {value: "E"}, {value: "E"}, {value: "E"}, {value: "E"}],
+            [{value: "E"}, {value: "E"}, {value: "E"}, {value: "E"}, {value: "E"}, {value: "E"}],
+            [{value: "E"}, {value: "E"}, {value: "E"}, {value: "E"}, {value: "E"}, {value: "E"}],
+            [{value: "E"}, {value: "E"}, {value: "E"}, {value: "E"}, {value: "E"}, {value: "E"}],
+            [{value: "E"}, {value: "E"}, {value: "E"}, {value: "E"}, {value: "E"}, {value: "E"}],
+            [{value: "E"}, {value: "E"}, {value: "E"}, {value: "E"}, {value: "E"}, {value: "E"}]];
+        this.player = "Order";
+        this.type = "standard";
+
+        this.webSocket = new WebSocket("ws://localhost:9000/socket");
+        webSocket.onopen = function () {
+            console.log("WebSocket opened");
+        };
+        webSocket.onmessage = function (message) {
+            console.log("WebSocket Message received");
+            console.log(JSON.parse(message.data));
+            this.loadFromJson(JSON.parse(message.data));
+        };
+        webSocket.onerror = function () {
+            console.error("WebSocket error");
+        };
+        webSocket.onclose = function () {
+            console.log("WebSocket closed");
+        };
+    }
+
+    loadFromJson(json) {
+        this.cells = (json.grid.fields).map(field => {
+            const cellType = field.type;
+            return cellType === "B" ? "blue-cell" : cellType === "R" ? "red-cell" : this.type === "standard" ? "empty-cell" : "empty-cell-blocked";
+        });
+        this.player = json.turn;
+        this.type = json.type;
+        this.updateTopText()
+    }
+
+    setCell(x, y, type) {
+        console.log("Setting cell (" + x + "," + y + ") to " + type);
+        const message = {x, y, type, "action": "set"};
+        this.sendMessage(message);
+    }
+
+    undo() {
+        console.log("Undoing last step");
+        const message = {"action": "undo"};
+        this.sendMessage(message);
+    }
+
+    redo() {
+        console.log("Redoing last step");
+        const message = {"action": "redo"};
+        this.sendMessage(message);
+    }
+
+    sendMessage(message) {
+        this.webSocket.send(JSON.stringify(message))
     }
 
     static get properties() {
         return {
             rows: { type: Array },
-            columns: { type: Array }
+            columns: { type: Array },
+            cells: {type: Array},
+            type: {type: String},
+            player: {type: String}
         };
     }
 
@@ -195,7 +254,7 @@ export class OrderAndChaos extends LitElement {
             <div class="container">
                 <div class="row">
                     <div class="col-sm-12">
-                        <h1 class="turn text-light" id="top-text">Turn: Order</h1>
+                        <h1 class="turn text-light" id="top-text">${this.type === "standard" ? "Turn: " : "Winner: " + this.player}</h1>
                     </div>
                 </div>
                 <div class="row">
@@ -203,130 +262,23 @@ export class OrderAndChaos extends LitElement {
                         ${this.rows.map(row => html`
                             <div class="game-row">
                                 ${this.columns.map(col => html `
-                                    <div class="game-cell" id="game-cell${col.value}${row.value}"></div>
+                                    <div class="game-cell ${this.cells[col.value][row.value]}" id="game-cell${col.value}${row.value}">
+                                        ${this.cells[col.value][row.value] === "empty-cell" && this.type === "standard"
+                                        ? html`<div class=\"choice red-choice\" @click="${this.setCell(col.value, row.value, "R")}"/>
+                                               <div class=\"choice blue-choice\" @click="${this.setCell(col.value, row.value, "B")}"/>`
+                                        : html``}
+                                    </div>
                                 `)}
                             </div>
                         `)}
                     </div>
                     <div class="side-buttons">
-                        <div class="button" id="undo-button">Undo</div>
-                        <div class="button" id="redo-button">Redo</div>
+                        <div class="button" id="undo-button" @click="${this.undo()}">Undo</div>
+                        <div class="button" id="redo-button" @click="${this.redo()}">Redo</div>
                     </div>
                 </div>
             </div>
-`;}
+    `;}
 }
 
 window.customElements.define('order-and-chaos', OrderAndChaos);
-
-let status;
-let webSocket;
-
-class Status {
-    constructor(websocket) {
-        this.loadFromJson(websocket)
-    }
-
-    loadFromJson(json) {
-        this.cells = json.grid.fields;
-        this.player = json.turn;
-        this.type = json.type;
-        this.updateCells();
-        this.updateTopText()
-    }
-
-    updateCells() {
-        for (const row in this.cells) {
-            for (const col in this.cells[row]) {
-                this.updateCell(row, col)
-            }
-        }
-    }
-
-    updateCell(x, y) {
-        const cellType = this.cells[y][x].type;
-        console.log("Updating cell (" + x + "," + y + ") to " + cellType);
-        const query = $("#game-cell" + x + y);
-        if (cellType !== "R") {
-            query.removeClass("red-cell");
-        }
-        if (cellType !== "B") {
-            query.removeClass("blue-cell");
-        }
-        if (cellType !== "E" || this.type !== "standard") {
-            query.removeClass("empty-cell")
-        }
-        if (cellType !== "E" || this.type !== "game-over") {
-            query.removeClass("empty-cell-blocked")
-        }
-
-        query.addClass(cellType === "B" ? "blue-cell" : cellType === "R" ? "red-cell" : this.type === "standard" ? "empty-cell" : "empty-cell-blocked");
-
-        if (cellType === "E" && this.type === "standard") {
-            query.addClass("empty-cell");
-            let html = "<div class=\"choice red-choice\"/>\n";
-            html = html + "<div class=\"choice blue-choice\"/>\n";
-            query.html(html);
-            query.children().eq(0).click(function () {
-                setCell(x, y, "R")
-            });
-            query.children().eq(1).click(function () {
-                setCell(x, y, "B")
-            });
-        } else {
-            query.empty();
-        }
-    }
-
-    updateTopText() {
-        const start = this.type === "standard" ? "Turn: " : "Winner: ";
-        $("#top-text").html(start + this.player)
-    }
-}
-
-function setCell(x, y, type) {
-    console.log("Setting cell (" + x + "," + y + ") to " + type);
-    const message = {x, y, type, "action": "set"};
-    sendMessage(message);
-}
-
-function undo() {
-    console.log("Undoing last step");
-    const message = {"action": "undo"};
-    sendMessage(message);
-}
-
-function redo() {
-    console.log("Redoing last step");
-    const message = {"action": "redo"};
-    sendMessage(message);
-}
-
-function sendMessage(message) {
-    webSocket.send(JSON.stringify(message))
-}
-
-$(document).ready(function () {
-    $("#undo-button").click(function () {
-        undo()
-    });
-    $("#redo-button").click(function () {
-        redo()
-    });
-    webSocket = new WebSocket("ws://localhost:9000/socket");
-    webSocket.onopen = function () {
-        console.log("WebSocket opened");
-    };
-    webSocket.onmessage = function (message) {
-        console.log("WebSocket Message received");
-        console.log(JSON.parse(message.data));
-        status = new Status(JSON.parse(message.data));
-    };
-    webSocket.onerror = function () {
-        console.error("WebSocket error");
-    };
-    webSocket.onclose = function () {
-        console.log("WebSocket closed");
-    };
-});
-
